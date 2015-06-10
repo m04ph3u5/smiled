87a0ad0c46216601adf44152c5a5f95b61f132a2
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2015-06-10 10:28:20 +0200

    Commit 10 giugno

---
+++ src/main/webapp/assets/public/js/controllers/personalProfileController.js
@@ -0,0 +1,40 @@
+angular.module('smiled.application').controller('personalProfileCtrl', ['$scope', 'apiService', 'Upload',
+                                                              function personalProfileCtrl($scope, apiService, Upload){
+	
+	//$scope.cover=null;
+	
+
+//	var uploadCover = function(file){
+//		console.log("uploadCover");
+//		$scope.cover=file;
+//		console.log($scope.cover);
+//		
+//		if($scope.cover!=null){
+//			var fd = new FormData();
+//			fd.append('file',$scope.cover);
+//			console.log(fd);
+//			apiService.uploadCoverScenario(id,$scope.cover);
+//		}
+//	}
+	
+	var uploadCover = function(file){
+		if(file && file.length){
+			Upload.upload({
+	            url: 'api/v1/media/users/cover/',
+	            headers : {
+	                'Content-Type': file.type
+	            },
+	            file: file
+	        })
+//	            .progress(function (evt) {
+//	            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+//	            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+//	        })
+	        .success(function (data, status, headers, config) {
+	            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+	        });
+		}
+	}
+	
+	$scope.uploadCover = uploadCover;
+}]);
