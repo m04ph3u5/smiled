7bc22f7430f54758f9249ed5739470119b2e7360
andrea <andrewgoodhelp88@gmail.com>
2015-07-07 15:57:27 +0200

    kpoko

--- src/main/webapp/assets/public/js/wrapping/stateChangeErrors.js
+++ src/main/webapp/assets/public/js/wrapping/stateChangeErrors.js
@@ -1,9 +1,8 @@
-angular.module('smiled.application').run(function($rootScope, alertingLogin){
+angular.module('smiled.application').run(function($rootScope){
 	//utile per catturare gli eventi che riguardano le transizioni di stato di ui-router
 
 		//quando ui-router fa scattare questo evento qui lo catturo
 		$rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error){
-			alertingLogin.addDanger("Impossibile caricare lo stato: "+ toState.name);
 			console.log("impossibile caricare lo stato " + toState.name);
 		});
 		
