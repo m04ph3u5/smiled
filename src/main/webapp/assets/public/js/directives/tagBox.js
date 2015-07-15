75f0674699f7a153703263b182cac5380ec210f0
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2015-07-15 15:58:49 +0200

    Work on tagBox directive

--- src/main/webapp/assets/public/js/directives/tagBox.js
+++ src/main/webapp/assets/public/js/directives/tagBox.js
@@ -1,4 +1,4 @@
-angular.module("smiled.application").directive('myTagBox', [ 'CONSTANTS', function(CONSTANTS){
+angular.module("smiled.application").directive('myTagBox', [ 'CONSTANTS', '$timeout', function(CONSTANTS, $timeout){
 	return {
 		restrict: 'EA',
 		templateUrl: 'assets/private/partials/tag-box-template.html',
@@ -13,29 +13,40 @@ angular.module("smiled.application").directive('myTagBox', [ 'CONSTANTS', functi
 			self.selectedIndex=-1;
 			self.suggestions = new Array();
 			self.searchText = "";
+			self.showSuggestions=false;
 			
 			self.search = function(){
+				self.suggestions = [];
+				self.selectedIndex=-1;
+
+				var regex = new RegExp("(^|\\s|-|'|,|\.)"+self.searchText,"gi");
 				if(self.selectable){
 					if(self.type=="user"){
 						for(var i=0; i<self.selectable.length; i++){
-							var suggestion = {};
-							suggestion.id=self.selectable[i].id;
-							suggestion.name=self.selectable[i].lastname+" "+self.selectable[i].firstname;
-							suggestion.cover=CONSTANTS.urlUserCover(self.selectable[i].id);
-							self.suggestions.push(suggestion);
+							if(regex.test(self.selectable[i].firstname) || regex.test(self.selectable[i].lastname)){
+								var suggestion = {};
+								suggestion.name=self.selectable[i].lastname+" "+self.selectable[i].firstname;
+								suggestion.id=self.selectable[i].id;
+								suggestion.cover=CONSTANTS.urlUserCover(self.selectable[i].id);
+								self.suggestions.push(suggestion);
+							}
 						}
 					}else if(self.type=="character"){
 						console.log("search->character");
+						console.log(regex.source);
+
 						if(!self.scenarioId){
 							throw new Error("Unsupported type");
 						}
 						for(var i=0; i<self.selectable.length; i++){
-							var suggestion = {};
-							suggestion.id=self.selectable[i].id;
-							suggestion.name=self.selectable[i].name;
-							suggestion.cover=CONSTANTS.urlCharacterCover(self.scenarioId,self.selectable[i].id);
-							self.suggestions.push(suggestion);
-							console.log("search->addSuggestion "+i);
+							if(regex.test(self.selectable[i].name)){
+								var suggestion = {};
+								suggestion.name=self.selectable[i].name;
+								suggestion.id=self.selectable[i].id;
+								suggestion.cover=CONSTANTS.urlCharacterCover(self.scenarioId,self.selectable[i].id);
+								self.suggestions.push(suggestion);
+								console.log("search->addSuggestion "+i);
+							}
 
 						}
 					}else
@@ -61,17 +72,32 @@ angular.module("smiled.application").directive('myTagBox', [ 'CONSTANTS', functi
 					event.preventDefault();
 					if(self.selectedIndex+1 !== self.suggestions.length){
 						self.selectedIndex++;
-					}
+					}else
+						self.selectedIndex=0;
 				}
 				else if(event.keyCode===38){
 					event.preventDefault();
 					if(self.selectedIndex-1 !== -1){
 						self.selectedIndex--;
-					}
+					}else
+						self.selectedIndex=self.suggestions.length-1;
 				}
 				else if(event.keyCode===13){
 					self.addToSelectedTags(self.selectedIndex);
 				}
+			}
+			
+			self.blur = function(){
+				console.log("BLUR");
+				$timeout(function(){
+					self.showSuggestions=false;
+				},100);
+				
+			}
+			
+			self.focus = function(){
+				console.log("FOCUS");
+				self.showSuggestions=true;
 			}
 		},
 		controllerAs: 'tagBox',
@@ -82,6 +108,14 @@ angular.module("smiled.application").directive('myTagBox', [ 'CONSTANTS', functi
 				if(val!==-1) {
 					controller.searchText = controller.suggestions[controller.selectedIndex].name;
 				}
+			});
+			
+			elem.on('focusout', function(){
+				controller.showSuggestions=false;
+			});
+			elem.on('focusin', function(){
+				controller.search();
+				controller.showSuggestions=true;
 			});
 		}
 	}
