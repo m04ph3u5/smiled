a6e0779728af807741369128e4a42c363135fbd7
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2015-07-17 16:01:39 +0200

    Add CONSTANTS utility for months and days

--- src/main/webapp/assets/public/js/controllers/dialogSetDateController.js
+++ src/main/webapp/assets/public/js/controllers/dialogSetDateController.js
@@ -1,6 +1,6 @@
-angular.module('smiled.application').controller('dialogSetDateCtrl', ['modalService','alertingGeneric', '$state', 'scen', 'start',
+angular.module('smiled.application').controller('dialogSetDateCtrl', ['modalService','alertingGeneric', '$state', 'scen', 'start', 'CONSTANTS',
        
-                                                                  function dialogSetDateCtrl(modalService, alertingGeneric, $state, scen, start){
+                                                                  function dialogSetDateCtrl(modalService, alertingGeneric, $state, scen, start, CONSTANTS){
 	
 	var self = this;
 	
@@ -11,18 +11,24 @@ angular.module('smiled.application').controller('dialogSetDateCtrl', ['modalServ
 	if(start==true){
 		//modifico la data di inizio
 		self.newDate = scen.history.startDate;
+		console.log(self.newDate);
 		
 	}else{
 		//modifico la data di fine
-		self.newDate = scen.history.endDate;
+		self.newDate = scen.history.endDate;		
+		console.log(self.newDate);
 	}
 	
 	
 	self.closeModal = function(){
 		modalService.closeModalSetDate();
 	}
-
 	
+	self.months = CONSTANTS.getMonths("it");
+	self.days = CONSTANTS.getDays(self.newDate.month);
+	self.getDays = function(){
+		self.days = CONSTANTS.getDays(self.newDate.month);
+	}
 	var checkDate = function(date){
 		// regular expression to match required date format
 //		   var re = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
--- src/main/webapp/assets/public/js/services/constantService.js
+++ src/main/webapp/assets/public/js/services/constantService.js
@@ -81,5 +81,80 @@ angular.module("smiled.application").constant("CONSTANTS",{
 		}
 		console.log(m);
 		return m;
+	},
+	"getMonths" : function(lang){
+		var months = new Array();
+		var month = {};
+		if(lang=="it"){
+			month = {value : 1, name: "gennaio"};
+			months.push(angular.copy(month));
+			month = {value : 2, name: "febbraio"};
+			months.push(angular.copy(month));
+			month = {value : 3, name: "marzo"};
+			months.push(angular.copy(month));
+			month = {value : 4, name: "aprile"};
+			months.push(angular.copy(month));
+			month = {value : 5, name: "maggio"};
+			months.push(angular.copy(month));
+			month = {value : 6, name: "giugno"};
+			months.push(angular.copy(month));
+			month = {value : 7, name: "luglio"};
+			months.push(angular.copy(month));
+			month = {value : 8, name: "agosto"};
+			months.push(angular.copy(month));
+			month = {value : 9, name: "settembre"};
+			months.push(angular.copy(month));
+			month = {value : 10, name: "ottobre"};
+			months.push(angular.copy(month));
+			month = {value : 11, name: "novembre"};
+			months.push(angular.copy(month));
+			month = {value : 12, name: "dicembre"};
+			months.push(angular.copy(month));
+		}else if(lang=="en"){
+			month = {value : 1, name: "January"};
+			months.push(angular.copy(month));
+			month = {value : 2, name: "February"};
+			months.push(angular.copy(month));
+			month = {value : 3, name: "March"};
+			months.push(angular.copy(month));
+			month = {value : 4, name: "April"};
+			months.push(angular.copy(month));
+			month = {value : 5, name: "May"};
+			months.push(angular.copy(month));
+			month = {value : 6, name: "June"};
+			months.push(angular.copy(month));
+			month = {value : 7, name: "July"};
+			months.push(angular.copy(month));
+			month = {value : 8, name: "Agoust"};
+			months.push(angular.copy(month));
+			month = {value : 9, name: "September"};
+			months.push(angular.copy(month));
+			month = {value : 10, name: "October"};
+			months.push(angular.copy(month));
+			month = {value : 11, name: "November"};
+			months.push(angular.copy(month));
+			month = {value : 12, name: "December"};
+			months.push(angular.copy(month));
+		}else
+			throw new Error("Not supported language");
+		
+		return months;
+	},
+	"getDays" : function(month){
+		var days = new Array();
+		var numDay;
+		if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+			numDay=31;
+		}else if(month==2){
+			numDay=28;
+		}else{
+			numDay=30;
+		}
+			
+		for(var i=1; i<=numDay; i++){
+			var day = {value : i};
+			days.push(day);
+		}
+		return days;
 	}
 });
