d466ab759d53ee8f36d2bf54f8b54973d2cd1e60
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2015-07-22 18:50:39 +0200

    Fix commentTo directive

--- src/main/webapp/assets/public/js/directives/commentTo.js
+++ src/main/webapp/assets/public/js/directives/commentTo.js
@@ -1,6 +1,6 @@
-angular.module("smiled.application").directive('commentTo',[ 
+angular.module("smiled.application").directive('commentTo',[ 'apiService',
 
-	function(){
+	function(apiService){
 		return {
 			templateUrl: "assets/private/partials/comment-to-template.html",
 			scope : {
@@ -10,7 +10,45 @@ angular.module("smiled.application").directive('commentTo',[
 				scenarioId : "@" 
 			},
 			controller : function(){
+				var self = this;
 				
+				self.addCommentToPost = function(){
+					console.log(self.post.newComment);
+					if(self.post.newComment){
+						var comment = {};
+						comment.text=self.post.newComment;
+						//aggiungo commento al post
+						apiService.sendCommentToPost(self.scenarioId, self.post.id, comment).then(
+								function(data){
+									//prendo dal server nuovamente il post a cui ho aggiunto il commento
+									apiService.getSingleStatus(self.scenarioId, self.post.id).then(
+											function(data){
+												self.post = data;
+												self.post.newComment="";
+//												for(var i=0; i<self.posts.length; i++){
+//													if(self.posts[i].id==data.id){
+//														data.newComment="";
+//														if(data.imageId)
+//															data.imageUrl = CONSTANTS.urlMedia(data.imageId);
+//														data.character.cover = CONSTANTS.urlCharacterCover(self.scen.id,data.character.id);
+//														//self.posts.splice(i,1,data);
+//														self.posts[i] = data;
+//														
+//													}
+//												}
+												
+											},
+											function(reason){
+												console.log("error in insert new post in array");
+											}
+									);
+								},
+								function(reason){
+									console.log("fail to send comment: "+reason);
+								}
+						);
+					}
+				}
 			},
 			controllerAs: "commentTo",
 			bindToController: true
