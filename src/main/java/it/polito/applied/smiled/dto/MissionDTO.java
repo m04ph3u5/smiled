924b6e77a38174281f38d6bc700a238c9ad99d4a
andrea <andrewgoodhelp88@gmail.com>
2015-07-29 20:29:03 +0200

    missionDTO aggiunto

---
+++ src/main/java/it/polito/applied/smiled/dto/MissionDTO.java
@@ -0,0 +1,56 @@
+package it.polito.applied.smiled.dto;
+
+import it.polito.applied.smiled.pojo.scenario.MissionStatus;
+
+import java.util.Date;
+
+public class MissionDTO {
+
+	private String teacherId;
+	private String studentId;
+	
+	private String title;
+	private String description;
+	
+	private Date deliveryDate;
+	private MissionStatus status;
+	
+	public String getTeacherId() {
+		return teacherId;
+	}
+	public void setTeacherId(String teacherId) {
+		this.teacherId = teacherId;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+	public MissionStatus getStatus() {
+		return status;
+	}
+	public void setStatus(MissionStatus status) {
+		this.status = status;
+	}
+	
+	
+}
