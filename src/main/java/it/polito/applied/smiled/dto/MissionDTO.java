1ee5ea50701bbdaa8c7cbea85733ee3a16efad3d
Francesca <n.francesca.milani@gmail.com>
2015-08-31 10:21:32 +0200

    commit prova

---
+++ src/main/java/it/polito/applied/smiled/dto/MissionDTO.java
@@ -0,0 +1,27 @@
+package it.polito.applied.smiled.dto;
+
+import java.util.Date;
+
+public class MissionDTO {
+
+	public String getStudentId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date getDeliveryDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
