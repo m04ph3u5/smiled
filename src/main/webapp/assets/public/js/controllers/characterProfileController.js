6bae227b6d0d42b25718a08af6b45a2fc5df40ed
Francesca <n.francesca.milani@gmail.com>
2015-09-11 19:01:18 +0200

    modifiche al profilo e loghi

--- src/main/webapp/assets/public/js/controllers/characterProfileController.js
+++ src/main/webapp/assets/public/js/controllers/characterProfileController.js
@@ -8,21 +8,19 @@ angular.module('smiled.application').controller('characterProfileCtrl', ['CONSTA
 	self.bornMonth = null;
 	self.deadMonth = null;
 	self.isOwner = false;
-	//mi serve il currentOwner per mettere l'immagine. Perché è undefined?
 	var currentOwnerId  = null;
 	var idUser = $scope.scenario.loggedUser.id;
 	self.actualUserCover = null;
-	//qua non dovrebbe avere il currentCharacter ereditato da scenario?
-	
+	self.newQuote = null;
 	
 	apiService.getCharacter(scenarioId, idChar).then(
 			function(data){
 				self.character = data;
-				//perché è undefined??
 				self.bornMonth = CONSTANTS.monthString(self.character.bornDate.month);
 				self.deadMonth = CONSTANTS.monthString(self.character.deadDate.month);
 				if(self.character.actualUser.id == idUser) self.isOwner = true;	
 				self.actualUserCover = CONSTANTS.urlUserCover(self.character.actualUser.id);
+				self.newQuote = self.character.quote;
 			},
 			function(reason){
 				console.log("ERROR RETRIEVE CHARACTER: ");
@@ -39,6 +37,25 @@ angular.module('smiled.application').controller('characterProfileCtrl', ['CONSTA
 			
 		//}		
 	}
+	
+	self.updateChar = function(){
+		console.log(self.newQuote);
+		var charDTO = {
+				'name': self.character.name,
+				'nickname': self.character.nickname,
+				'description' : self.character.bornDate,
+				'bornDate': self.character.bornDate,
+				'deadDate': self.character.deadDate,
+				'cover': self.character.deadDate,
+				'quote': self.newQuote,
+				'gender': self.character.gender,
+				'role': self.character.role,
+				'bornTown': self.character.bornTown,
+				'deadTown': self.character.deadTown
+		}
+//		apiService.updateCharacter(scenarioId, charDTO , idChar);
+	}
+	
 //	self.getUserCover = function(){
 //		console.log("STO CERCANDO"+id)
 //		self.userCover = CONSTANTS.urlUserCover(id);				
