b39ea8ae2aa8bc994d2ae5e4c1b18bf1efa00f4e
Francesca <n.francesca.milani@gmail.com>
2015-10-05 12:02:31 +0200

    Modal compiti

---
+++ src/main/webapp/assets/public/js/controllers/dialogMissionController.js
@@ -0,0 +1,30 @@
+angular.module('smiled.application').controller('dialogMissionCtrl', ['modalService', 'apiService', 'alertingGeneric',
+                 function dialogMissionCtrl(modalService, apiService, alertingGeneric){
+	var self = this;
+	self.mission = {};
+	
+	self.createMission = function (scenId){
+		if(scenId == null){
+			//compito creato dalla dashboard
+			apiService.createMission(self.mission.scenId, self.mission).then(
+					function(data){
+//						self.mission.title="";
+//						self.mission.description="";
+//						self.mission.deliverydate=null;
+//						self.mission.studentId="";
+						
+						 //alertingGeneric.addSuccess("MissionCreata");
+						 modalService.closeModalCreateMission();
+					},
+					function(reason){
+						
+						console.log("error in creation of new mission");
+					}
+			);
+		}else{
+			//compito creato dallo scenario
+			
+		}
+		
+	}
+}]);
