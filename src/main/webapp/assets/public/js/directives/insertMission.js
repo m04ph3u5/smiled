1bed3d6c8cba87864b0f897aa85d721e33cb17ad
andrea <andrewgoodhelp88@gmail.com>
2015-10-30 19:45:00 +0100

    missioni complete

--- src/main/webapp/assets/public/js/directives/insertMission.js
+++ src/main/webapp/assets/public/js/directives/insertMission.js
@@ -1,5 +1,5 @@
-angular.module("smiled.application").directive("insertMission", [ 'CONSTANTS', 'apiService', 'Upload', '$q', 'modalService',
-                                     function(CONSTANTS, apiService, Upload, $q, modalService){
+angular.module("smiled.application").directive("insertMission", [ 'CONSTANTS', 'apiService', 'Upload', '$q', 'modalService', '$timeout',
+                                     function(CONSTANTS, apiService, Upload, $q, modalService, $timeout){
 	return {
 		templateUrl: "assets/private/partials/insert-mission-template.html",
 		scope : {
@@ -12,48 +12,73 @@ angular.module("smiled.application").directive("insertMission", [ 'CONSTANTS', '
 		bindToController: true,
 		controller: ['$scope', function(){
 			var self = this;
+			self.dirty = false;
 			self.realDateFormat = CONSTANTS.realDateFormatWithHour;
 			if(self.character){
 				if(self.character.mission){
 					self.mission= self.character.mission;
+					self.newMission = angular.copy(self.mission);
 					
-				}else
-					self.mission={};
+				}else{
+					self.mission=null;
+					self.newMission = null;
+				}
 				self.urlCover = "url('"+CONSTANTS.urlCharacterCover(self.scenario.id,self.character.id)+"'),url('assets/public/img/icon/pg.png')";
 			}else{
 				self.urlCover = "url('"+CONSTANTS.urlScenarioCover(self.idScenarioVeloce)+"'),url('assets/public/img/icon/ic_scen.png')";
 				
 				if(self.scenario.mission){
 					self.mission= self.scenario.mission;
-				}else
-					self.mission={};
+					self.newMission = angular.copy(self.mission);
+				}else{
+					self.mission=null;
+					self.newMission=null;
+				}
+					
 			}
 			
-			
+			console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			console.log(self.mission);
 			self.saveMission = function(){
 				if(self.character){
 					console.log("saving mission character...");
-					console.log(self.mission);
-					apiService.addMissionToCharacter(self.scenario.id, self.character.id, self.mission).then(
+					console.log(self.newMission);
+					apiService.addMissionToCharacter(self.scenario.id, self.character.id, self.newMission).then(
 							function(data){
 								self.character = data;
+								self.mission = self.character.mission;
+								self.newMission = angular.copy(self.mission);
+								self.dirty=false;
 							},
 							function(reason){
 								console.log("error in add mission to character");
+								self.newMission = angular.copy(self.mission);
+								self.dirty=false;
 							}
 					);
 				}else{
 					console.log("saving mission scenario...");
 					console.log(self.mission);
-					apiService.addMissionToScenario(self.scenario.id, self.mission).then(
+					apiService.addMissionToScenario(self.scenario.id, self.newMission).then(
 							function(data){
 								self.scenario = data;
+								self.mission = self.scenario.mission;
+								self.newMission = angular.copy(self.mission);
+								self.dirty=false;
 							},
 							function(reason){
 								console.log("error in add mission to scenario");
+								self.newMission = angular.copy(self.mission);
+								self.dirty=false;
 							}
 					);
 				}
+				
+			}
+			
+			self.deleteModifyMission = function(){
+				self.newMission = angular.copy(self.mission);
+				$timeout(function(){self.dirty=false;},200);
 				
 			}
 			
@@ -62,7 +87,15 @@ angular.module("smiled.application").directive("insertMission", [ 'CONSTANTS', '
 		}],
 		controllerAs: "insertMission",
 		link : function(scope,elem,attrs,ctrl){
-
+			scope.$watch('insertMission.newMission.title', function(newVal, oldVal){
+				if(newVal && newVal!=oldVal)
+					ctrl.dirty=true;
+			});
+			
+			scope.$watch('insertMission.newMission.description', function(newVal, oldVal){
+				if(newVal && newVal!=oldVal)
+					ctrl.dirty=true;
+			});
 		}
 	}
 }]);
