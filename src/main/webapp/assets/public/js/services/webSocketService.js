376a0f76d30a142c18e9537ed145690c87cb989e
andreabuonaiuto <andreabuonaiuto@192.168.1.44>
2016-02-25 11:25:37 +0100

    path assoluto per websocket

--- src/main/webapp/assets/public/js/services/webSocketService.js
+++ src/main/webapp/assets/public/js/services/webSocketService.js
@@ -8,7 +8,7 @@ angular.module('smiled.application').factory('webSocketService', [ '$timeout','m
 	
 	 service.RECONNECT_TIMEOUT = 30000;
 	 service.START_AFTER_TIME_TO_SETUP=1500;
-	 service.SOCKET_URL = "https://localhost:8443/websocket/messages";
+	 service.SOCKET_URL = "/websocket/messages";
 	 //service.CHAT_TOPIC = "/topic/message";
 	 //service.CHAT_BROKER = "/app/chat";
 	 
