7ae1833c3775601c0a8a62679a9ecf8e93f4f84e
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2016-03-08 18:20:08 +0100

    Add exponential backoff retry on websocket service (js)

--- src/main/webapp/assets/public/js/services/webSocketService.js
+++ src/main/webapp/assets/public/js/services/webSocketService.js
@@ -4,13 +4,28 @@ angular.module('smiled.application').factory('webSocketService', [ '$timeout','m
 	var service = {};
 	
 	var socket = {};
-	var reconnectAttempt=0;
+	var exp=0;
 	
-	 service.RECONNECT_TIMEOUT = 30000;
-	 service.START_AFTER_TIME_TO_SETUP=700;
-	 service.SOCKET_URL = "/websocket/messages";
-	 //service.CHAT_TOPIC = "/topic/message";
-	 //service.CHAT_BROKER = "/app/chat";
+	service.RECONNECT_TIMEOUT = 1000;
+	service.START_AFTER_TIME_TO_SETUP=700;
+	service.SOCKET_URL = "/websocket/messages";
+	//service.CHAT_TOPIC = "/topic/message";
+	//service.CHAT_BROKER = "/app/chat";
+	 
+	var getTimeout = function(){
+		var base;
+		if(exp!=0)
+			base=exp*service.RECONNECT_TIMEOUT;
+		else
+			base=service.RECONNECT_TIMEOUT;
+		
+		return base+(Math.random()*1000);
+	}
+	
+	var resetTimeout = function(){
+		console.log("ON OPEN!!!!!!!!");
+		exp=0;
+	}
 	
 	//Data usually are ACK_N, VIEW_N, ACK_M, VIEW_M, or USER_MESSAGE 
 	var send = function(data){
@@ -21,26 +36,33 @@ angular.module('smiled.application').factory('webSocketService', [ '$timeout','m
 	var reconnect = function() {
 	      $timeout(function() {
 	        retry();
-	      }, service.RECONNECT_TIMEOUT);  //aggiungere un meccanismo di backoff esponenziale per la riconnessione
+	      }, getTimeout());  //aggiungere un meccanismo di backoff esponenziale per la riconnessione
 	};
 	
 	var retry = function(){
-		console.log("Try to reconnect. Attempt number "+reconnectAttempt);
-		reconnectAttempt+1;
+		if(exp==0)
+			exp=1;
+		else if(exp<256)
+			exp=exp*2;
 		initialize();
 	}
 	
 	var initialize = function() {
 		socket = new SockJS(service.SOCKET_URL);
+		console.log(socket);
+
 		console.log('open ws connection on webSocketService');
+		
+		socket.onopen = function(){
+			resetTimeout();
+		}
+		
 		socket.onmessage = function(e) {
-	 	   var msg = JSON.parse(e.data);
-	 	   reconnectAttempt=0;
+	 	   	var msg = JSON.parse(e.data);
 	 	    if(msg.type=="NOTIFICATION"){ 	    	
 	 	    	notifyService.newNotifyOrPost(msg);
 	 	    }else{
 	 	    	console.log("message is a message");
-	 	    	//messageService.newMessage(e.data);
 	 	    }
 		};
 		
