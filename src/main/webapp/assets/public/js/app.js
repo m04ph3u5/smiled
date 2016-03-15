1cc6d9088d01d5e278748e2591ac309c1a08a54b
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2016-03-15 17:00:19 +0100

    Various fix. Duplicate index

--- src/main/webapp/assets/public/js/app.js
+++ src/main/webapp/assets/public/js/app.js
@@ -1,6 +1,6 @@
 angular.module('smiled.application', ['ui.router', 'ngCookies', 'ui.bootstrap', 
                                       'ngStorage', 'ngResource', 'permission',
-                                      'restangular', 'ngFileUpload', 'ui.date', 'ngDialog',
+                                      'ngFileUpload', 'ui.date', 'ngDialog',
                                       'ang-drag-drop','infinite-scroll', 'ngTagsInput',
                                       'bootstrapLightbox', 'ngSanitize', 'fox.scrollReveal',
                                       'ui.slider', 'FBAngular', 'angular-p5']);
